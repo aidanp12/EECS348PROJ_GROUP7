1a7cc1fc638f7ea3e8d351aaed96397c81af1a87
aidanp12 <aidanp1230@icloud.com>
2024-12-09 02:25:21 -0600

    DataStorage1.1
    Updated version of DataStorage, now with the ExpressionTree having full functionality and an added Results() class to keep track of previous data. main function demonstrates how the class can be used.

--- scripts/DataStorageModule/DataStorage.cpp
+++ scripts/DataStorageModule/DataStorage.cpp
@@ -1,17 +1,20 @@
 // DataStorage.cpp : This file contains the primary components for the DataStorage module. 
 // ExpressionTree() : a node-based class structure that saves & computes a prefix-notation expression.
 // Node : a struct designed to link together expressions and numbers within the ExpressionTree.
-// Memory : allows for storage and recall of results. 
+// Results() : allows for storage and recollection of results. 
 
-/* NOTE: This is not the final version, still working on it + 
-adding functionality & finishing touches, just wanted something 
-on the github to help with implementation. will be finished soon 
-(03.12.24)
+/* NOTE: This is an implementable version of the data storage module. The main function demonstrates
+* how each method of the classes should be used. It could do for some finishing touches but I figured 
+* it is far functional enough as is to consider it "finished". 
+(09.12.24)
 */
 
 
 #include <iostream>
 #include <string>
+#include <cmath>
+#include <vector>
+
 
 
 // Binary Node structure makes up the expression tree
@@ -28,118 +31,196 @@ struct Node {
 
 
 class ExpressionTree {
-    // ExpressionTree is used to evaluate any given input in Prefix/Polish notation.
-    // It's not actually a tree but rather a stack. Need to update documentation correspondingly.
-    // Courtesy to Wikipedia and EECS210 for teaching me about prefix notation.
-    private: 
-       Node* top = nullptr;
-       std::string originalExpression;
+// ExpressionTree is used to evaluate any given input in Prefix/Polish notation.
+// It's not actually a tree but rather a stack. Need to update documentation correspondingly.
+// Courtesy to Wikipedia and EECS210 for teaching me about prefix notation.
+private: 
+    Node* top = nullptr;
+    std::string originalExpression;
 
-    public:
-        // Constructor
-        ExpressionTree(std::string expression) {
-            buildExpression(expression);
-            originalExpression = expression;
-        }
+public:
+    // Constructor
+    ExpressionTree(std::string expression) {
+        buildExpression(expression);
+        originalExpression = expression;
+    }
 
-        void buildExpression(std::string formattedExpression) {
-            // The formattedExpression should be in Polish notations seperated by whitespaces. (ex: [* - 5 16 -7] )
-            // Read in a string from right to left 
-            for (int i = formattedExpression.length() - 1; i >= 0; --i) {
+    // Destructor 
+    ~ExpressionTree() {
+        delete top;
+    }
+    void buildExpression(std::string formattedExpression) {
+        // The formattedExpression should be in Polish notations seperated by whitespaces. (ex: [* - 5 16 -7] )
+        // Read in a string from right to left 
+        for (int i = formattedExpression.length() - 1; i >= 0; --i) {
 
-                char c = formattedExpression[i];
+            char c = formattedExpression[i];
 
-                // Skip the spaces
-                if (c == ' ') {
-                    continue;
-                }
-
-                // check if the character is non-numeric (an operator) by referencing its ASCII value 
-                if (c - '0' < 0 || c - '0' > 9) {
-                    // Pushes the operator to the stack
-                    std::string operatorVal = "";
-                    operatorVal += c;
-                    push(operatorVal);
-                    top->isOperator = true;
-                }
-
-                // handle numeric inputs
-                else {
-                    int next = 0;
-                    std::string nodeVal = "";
-                    nodeVal += c;
-                    // keep checking right to left after a number to determine if there are multiple digits
-                    while (formattedExpression[i - (next + 1)] != ' ' && i - (next + 1) >= 0) {
-                        nodeVal = formattedExpression[i - (next+1)] + nodeVal;
-                        next++;
-                    }
-                    push(nodeVal);
-                    i -= next;
-                }
-
+            // Skip the spaces
+            if (c == ' ') {
+                continue;
             }
-        }
 
-        int evaluate() {
-            // not sure if i supposed to do this but it seems pretty easy to incorporate right into the class
-            // idk may look at it tmr/later
-            while (returnTop() == 1) {
-                
+            // check if the character is non-numeric (an operator) by referencing its ASCII value 
+            if (c - '0' < 0 || c - '0' > 9) {
+                // Pushes the operator to the stack
+                std::string operatorVal = "";
+                operatorVal += c;
+                push(operatorVal);
+                top->isOperator = true;
             }
-        }
 
-        void handleOperator() {
-            // handle operators recursively 
-            /* note for when its not 2 AM: keep checking 2 nodes deep until both next 2 nodes
-            are numbers, then continue returning results up the expression tree. prefix notation is 
-            cool that way so it handles itself as long as valid prefix notation is given in the input*/
-        };
-
-        // Adds a value to the top of the stack
-        void push(std::string add_val) {
-            Node* newEntry = new Node(add_val);
-            if (top == nullptr) {
-                top = newEntry;
-            }
+            // handle numeric inputs
             else {
-                newEntry->next = top;
-                top = newEntry;
+                int next = 0;
+                std::string nodeVal = "";
+                nodeVal += c;
+                // keep checking right to left after a number to determine if there are multiple digits
+                while (formattedExpression[i - (next + 1)] != ' ' && i - (next + 1) >= 0) {
+                    nodeVal = formattedExpression[i - (next+1)] + nodeVal;
+                    next++;
+                }
+                push(nodeVal);
+                i -= next;
             }
-        }
 
-        std::string pop() {
-            if (top != nullptr) {
-                std::string topVal = top->data;
-                Node* temp = top->next;
-                top = temp;
-                return topVal;
-            }
-            else {
-                // throw an exception for when no top value is present to be removed, represented by code the 100
-                throw 100;
-            }
         }
+    }
+
+    double evaluate() {
+        // The formattedExpression should be in Polish notations seperated by whitespaces. (ex: [* - 5 16 -7] )
+        // Read in a string from right to left 
+        top = nullptr;
+        for (int i = originalExpression.length() - 1; i >= 0; --i) {
+
+            char c = originalExpression[i];
+
+            // Skip the spaces
+            if (c == ' ') {
+                continue;
+            }
+
+            // check if the character is non-numeric (an operator) by referencing its ASCII value 
+            if (c - '0' < 0 || c - '0' > 9) {                 
+                // Establish previous values to operate with
+                double result = 0;
+                double num1 = std::stod(pop());
+                double num2 = std::stod(pop());
+
+                // Define behaviors for each potential operator
+                switch (c) {
+                    case '+':
+                        result = num1 + num2; 
+                        break;
+                    case '-':
+                        result = num1 - num2; 
+                        break;
+                    case '*':
+                        result = num1 * num2; 
+                        break;
+                    case '/':
+                        result = num1 / num2;
+                        break;
+                    case '%':
+                        result = std::fmod(num1, num2);
+                        break;
+                    case '^':;
+                        result = std::pow(num1, num2);
+                        break;
+
+                }
+                // add the new value to the top of the expression tree
+                push(std::to_string(result));
+            }
+
+            // handle numeric inputs
+            else {
+                int next = 0;
+                std::string nodeVal = "";
+                nodeVal += c;
+                // keep checking right to left after a number to determine if there are multiple digits
+                while (originalExpression[i - (next + 1)] != ' ' && i - (next + 1) >= 0) {
+                    nodeVal = originalExpression[i - (next + 1)] + nodeVal;
+                    next++;
+                }
+                push(nodeVal);
+                i -= next;
+            }
+
+        }
+        return std::stod(pop());
+    }
+
+    // Adds a value to the top of the stack
+    void push(std::string add_val) {
+        Node* newEntry = new Node(add_val);
+        if (top == nullptr) {
+            top = newEntry;
+        }
+        else {
+            newEntry->next = top;
+            top = newEntry;
+        }
+    }
+
+    // Remove & return the top value of the stack
+    std::string pop() {
+        if (top != nullptr) {
+            std::string topVal = top->data;
+            Node* temp = top->next;
+            top = temp;
+            return topVal;
+        }
+        else {
+            // throw an exception for when no top value is present to be removed, represented by code the 100
+            throw 100;
+        }
+    }
         
-        // Print the top value to the terminal (should only be used for debugging) 
-        int returnTop() {
-            if (top != nullptr) {
-                std::cout << "Top data: " << top->data << std::endl;
-                return 1;
-            }
-            else {
-                std::cout << "Nothing in the ExpTree." << std::endl;
-                return 0;
-            }
+    // Print the top value to the terminal (should only be used for debugging) 
+    int returnTop() {
+        if (top != nullptr) {
+            std::cout << "Top data: " << top->data << std::endl;
+            return 1;
         }
+        else {
+            std::cout << "Nothing in the ExpTree." << std::endl;
+            return 0;
+        }
+    }
 
         
 };
 
 
-// yeah i didnt get this far but that's on the to-do list tomorrow (03.12)
-// this will be responsible for storing outputs/results and such, maybe it'll hold stuff like the original expression
-// but honestly i kinda realized it was pointless beyond storing results which can just be done in a vertex but who knows
-class Results {};
+// stores previous results and allows for easy access to previous results.
+// I really wanted to try and make timestamps for each result, but i kept running into an "unsafe function" -
+// - issue, couldnt figure it out. may come back to this later (09.12)
+class Results {
+private:
+    // variables
+    std::vector<double> results;
+
+public:
+    // Constructor
+    Results() {}
+
+    double lastResult() {
+        return results[results.size() - 1];
+    }
+
+    void record(double result) {
+        // store result in vector
+        results.push_back(result);
+    }
+
+    // this is really designed for the "outside compiler model implementation" than anything
+    void displayResults() {
+        for (int i = 0; i < results.size(); i++) {           
+            std::cout << "Result: " << results[i] << std::endl;
+        }
+    }
+};
 
 
 int main()
@@ -148,12 +229,15 @@ int main()
     have one. */
 
     // Example of using the ExpressionTree class
-    ExpressionTree tree("* - 5 6 7");
-
-    // Push operands to the tree
-
+    ExpressionTree tree("- * + 6 2 10 / 12 4");
+    Results results;
 
     // Print top node (last operator or operand added)
     while (tree.returnTop()) tree.pop();
+
+    // Example of recording/printing results to 
+    results.record(tree.evaluate());
+    results.displayResults();
+
     return 0;
 }
