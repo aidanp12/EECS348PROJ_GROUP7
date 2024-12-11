497e6a3515d5bbb73a4bf990e577a43e46d6de74
Mathai <a883m784@home.ku.edu>
2024-12-11 15:53:40 -0600

    added while loop to continue the program

--- scripts/aee_root/main_aee.cpp
+++ scripts/aee_root/main_aee.cpp
@@ -4,9 +4,26 @@
 #include <iostream>
 #include "input.cpp"  // use input module
 
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+
 int main() 
 {
-	Input input(1);
-
-	return 0;
+	bool continue_program = true;
+    
+    while (continue_program) {
+        cout << "Do you want to continue the program? Type \"y\" for yes or \"n\" for no: ";
+        
+        char response;
+        cin >> response;
+        
+        if (response == 'n' || response == 'N') {
+            continue_program = false;
+        } 
+        // If user types 'y', the loop just continues.
+    }
+    
+    return 0;
 }
